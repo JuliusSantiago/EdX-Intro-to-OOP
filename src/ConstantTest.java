fe3e8ba11e7b221714794292652367206b53c9b2
Julius Santiago <exotic_dancing_monkey@hotmail.com>
2025-05-11 22:52:09 -0700

    added a comment

--- src/ConstantTest.java
+++ src/ConstantTest.java
@@ -1,6 +1,6 @@
 public class ConstantTest {
     public static void main(String[] args) {
         final double PI = 3.14159265359;
-        PI = 3.14;
+        PI = 3.14; //can't reassign the variable
     }
 }
