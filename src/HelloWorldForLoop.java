0809fc458706bc8d498ce86f75233dce3d4c261e
Julius Santiago <exotic_dancing_monkey@hotmail.com>
2025-05-27 22:32:58 -0700

    bunch of module 3, lesson 7 stuff

---
+++ src/HelloWorldForLoop.java
@@ -0,0 +1,7 @@
+public class HelloWorldForLoop {
+    public static void main(String[] args) {
+        for(int helloNum = 0; helloNum < 10; helloNum++){
+            System.out.println("Hello World! " + (helloNum + 1));
+        }
+    }
+}
