a3f6c9c2c8913fa88f05470b17e8261d41bf0cb7
Julius Santiago <exotic_dancing_monkey@hotmail.com>
2025-06-05 01:07:44 -0700

    hw02

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,83 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("This program will take inputs to add, subtract, divide, multiply, and ")
+        System.out.println("List of operations: add subtract multiply divide alphabetize");
+        Scanner input = new Scanner (System.in);   //create Scanner object
+        System.out.println("Enter an operation: ");
+        String selection = input.next();   //get user input for operation to perform
+        String error = "Invalid input entered. Terminating..."; //error message
+
+        switch(selection.lowerSelection){
+            case "add":
+                System.out.println("Enter two integers: ");
+                //check for valid input, looking for two integers
+                if(input.hasNextInt()){
+                    int input1 = input.nextInt();
+                    if(input.hasNextInt()){
+                        int input2 = input.nextInt();
+                        System.out.println("Answer: " + (input1 + input2));
+                    } else{
+                        System.out.println(error);
+                    }
+                } else{
+                    System.out.println(error);
+                }
+                break;
+            case "subtract":
+                System.out.println("Enter two integers: ");
+                //check for valid input, looking for two integers
+                if(input.hasNextInt()){
+                    int input1 = input.nextInt();
+                    if(input.hasNextInt()){
+                        int input2 = input.nextInt();
+                        System.out.println("Answer: " + (input1 - input2));
+                    } else{
+                        System.out.println(error);
+                    }
+                } else{
+                    System.out.println(error);
+                break;
+            case "multiply":
+                System.out.println("Enter two doubles: ");
+                //check for valid input, looking for two integers
+                if(input.hasNextDouble()){
+                    double input1 = input.nextDouble();
+                    if(input.hasNextDouble()){
+                        double input2 = input.nextDouble();
+                        System.out.printf("Answer: %.2f\n" + (input1 * input2));
+                    } else{
+                        System.out.println(error);
+                    }
+                } else{
+                    System.out.println(error);
+                }
+                break;
+            case "divide":
+                System.out.println("Enter two doubles: ");
+                //check for valid input, looking for two integers
+                if(input.hasNextDouble()){
+                    double input1 = input.nextDouble();
+                    if(input.hasNextDouble()){
+                        double input2 = input.nextDouble();
+                        if(input2 != 0){
+                            System.out.printf("Answer: %.2f\n" + (input1 * input2));
+                        } else{
+                            System.out.println(error);
+                        }
+                    } else{
+                        System.out.println(error);
+                    }
+                } else{
+                    System.out.println(error);
+                }
+                break;
+            case "alphabetize":
+            
+                break;
+            default:
+                break;
+        }
+
     }
 }
